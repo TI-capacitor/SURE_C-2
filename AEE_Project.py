ac2b4d2164d983f61ae90aefe46ad8bd0edebd04
TI-capacitor <danielbarreiro014@gmail.com>
2025-03-09 12:07:32 -0400

    This is the work I made at the SURE C^2 intership

---
+++ AEE_Project.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy
+#import sqlite3
+#conn = sqlite3.connect('C:\Users\danie\OneDrive\Desktop\AEE Generation')
+#data = pd.read_csv("C:/Users/danie/OneDrive/Desktop/AEE_Generation.csv", encoding='utf-8')
+df = pd.read_csv('AEE_Generation_4.csv',delimiter= ',', encoding= 'latin-1')
+df.head()
